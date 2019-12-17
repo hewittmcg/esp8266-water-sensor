f7b2c86b04a28b50c028d6ed741a993cba759bea
hewittmcg <hewittmcg@users.noreply.github.com>
2019-12-17 18:38:31 -0500

    Create Water_Sensor.ino

---
+++ Water_Sensor/Water_Sensor.ino
@@ -0,0 +1,12 @@
+//just placeholder code to initialize with github
+//testing 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
